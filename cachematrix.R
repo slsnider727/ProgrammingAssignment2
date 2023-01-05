9386e86035be4336d9d9f0e07f000944b1a91c92
sabrasnider <sabra.snider@s2shape.com>
2023-01-05 00:07:39 -0500

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Comments that describe WHAT code does are useless. If the code is written
+## well, descriptions are unnecessary, take up space, and cause mess.
 
-## Write a short comment describing this function
+## But sure, let's be bad programmers. This function creates a special "matrix" object
+## that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  list(set = set, 
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix 
+## if it doesn't exist, else retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse()
+        if(!is.null(i)) {
+          message("getting cached data")
+          return(i)
+        }
+        data <- x$get()
+        i <-solve(data)
+        x$setInverse(i)
+        print(i)
 }
